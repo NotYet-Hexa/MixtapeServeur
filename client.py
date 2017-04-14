d05cb589d9e5fe858bacebb34bc16964cb99acb1
ChristopherAparicio <christopher.aparicio@free.fr>
2017-04-14 09:48:16 +0200

    Update Raspberry Registration

--- client.py
+++ client.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from socketIO_client import SocketIO, BaseNamespace
+from socketIO_client import SocketIO, BaseNamespace, LoggingNamespace
+import json
+
+def registrationResponse(*args):
+    print('raspberry_registration', args)
+
+data = {	
+		"raspberry":"gaston_berger"
+	}
+
+json_data_registration = json.dumps(data)
 
 socketIO = SocketIO('localhost', 8080)
-socketIO.emit('message', 'emit_data')
+socketIO.on('raspberry_registration',json_data_registration)
 socketIO.wait()
